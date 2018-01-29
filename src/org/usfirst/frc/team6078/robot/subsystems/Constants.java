d857c9667b67f4b8379ecb57c02163edab51221e
Jajoo <hersheyman123@hotmail.com>
2018-01-29 11:13:33 -0500

    This is the turbo button changes and also hopefully the fixed merging issues, we'll see next practice lol

--- src/org/usfirst/frc/team6078/robot/subsystems/Constants.java
+++ src/org/usfirst/frc/team6078/robot/subsystems/Constants.java
@@ -2,18 +2,14 @@ package org.usfirst.frc.team6078.robot.subsystems;
 
 public class Constants {
 	
+	//Below is a comment from the first year, it is a relic and must stay here forever as a memory
 	//Drivetrain motor ports on rio, numbers are funky because Kelsey wired them wrong. Thanks Kelsey :D
-<<<<<<< HEAD
-	public static int frontLeftMotorPort = 3;
-	public static int frontRightMotorPort = 0;
-	public static int backLeftMotorPort = 2; 
-	public static int backRightMotorPort = 1;
-=======
+
 	public static int frontLeftMotorPort = 1;
 	public static int frontRightMotorPort = 2;
 	public static int backLeftMotorPort = 0; 
 	public static int backRightMotorPort = 3;
->>>>>>> 24d4d2d4550806fc8512abf329687d5a8edb19c3
+
 	
 	//Joysticks 
 	public static int xboxPort = 0;
--- src/org/usfirst/frc/team6078/robot/subsystems/Drivetrain.java
+++ src/org/usfirst/frc/team6078/robot/subsystems/Drivetrain.java
@@ -18,15 +18,8 @@ public class Drivetrain {
 	
 	public static class  drivetrainV1 {
 	
-		public static DifferentialDrive drive = new DifferentialDrive(RobotMap.LeftMotors, RobotMap.RightMotor);
+		public static DifferentialDrive drive = new DifferentialDrive(RobotMap.LeftMotors, RobotMap.RightMotors);
 	
-	}
-	
-	public static class drivetrainV2 {
-		public static DifferentialDrive driveLeft = new DifferentialDrive(RobotMap.frontLeftMotor, RobotMap.backLeftMotor); 
-		
-		public static DifferentialDrive driveRight = new DifferentialDrive(RobotMap.frontRightMotor, RobotMap.backRightMotor);
-		
 	}
 	
 	
@@ -70,43 +63,6 @@ public class Drivetrain {
 				
 			}
 			
-	}
-
-	//Change this to a while loop dummy, that's why it only works once
-	 public static void turboButton() {
-		
-		boolean buttonValue;
-		
-		buttonValue = OI.operatorJoystick.getRawButton(3);
-		
-		if (buttonValue == true) {
-			
-			Constants.handicap = -1;
-		}
-		
-		else if (buttonValue = false) {
-			
-			Constants.handicap = -1.4;
-			
-		}
-		
-	}
-	
-	 
-	 //If Handicap is added back in will this work?
-	public static void switchDirection() {
-		
-		boolean buttonValue3;
-		
-		buttonValue3 = OI.operatorJoystick.getRawButton(2);
-		
-		while (buttonValue3 == true) {
-			
-			Constants.handicap = 1.4;
-			
-		}
-	
-		
 	}
 	
 }
